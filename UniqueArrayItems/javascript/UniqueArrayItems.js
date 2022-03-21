b83d4eeda9403fd70356aa2def744545c5526cca
Robert Michelis <robert.michelis@croftertree.com>
2022-03-20 23:17:18 -0400

    Count values and see if each value is unique

--- UniqueArrayItems/javascript/UniqueArrayItems.js
+++ UniqueArrayItems/javascript/UniqueArrayItems.js
@@ -1,6 +1,25 @@
-
+console.log(UniqueArrayItems([1,1,2,3,4,4,5,5,5,-9]));
+console.log(UniqueArrayItems([1,2,3,4,5]));
 
 function UniqueArrayItems(inArray) {
+    let hash = {};
+    let retVal = true;
 
-    
+    // Count and store the values into a hash
+    inArray.forEach(element => {
+        hash[element] = hash[element] ? hash[element] + 1 : 1;
+    });
+
+    console.log(hash);
+
+    // Now see if the values are unique
+    for (const [key, value] of Object.entries(hash)) {
+        console.log(key, value);
+        if (value > 1) {
+            retVal = false;
+            break;
+        }
+    }
+
+    return retVal;
 }
