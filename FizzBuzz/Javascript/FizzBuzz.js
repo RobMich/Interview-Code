a0af9444055460882aaab944ab770221bf413a33
Robert Michelis <robert.michelis@croftertree.com>
2022-03-22 22:19:01 -0400

    FizzBuzz Javascript implementation

---
+++ FizzBuzz/Javascript/FizzBuzz.js
@@ -0,0 +1,20 @@
+
+RunFizzBuzz([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]);
+
+function RunFizzBuzz(inputArray) {
+    inputArray.forEach(element => {
+        FizzBuzz(element);
+    });
+}
+
+function FizzBuzz(num) {
+    if (num % 3 === 0 && num % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (num % 3 === 0) {
+        console.log("Fizz");
+    } else if (num % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(num);
+    }
+} 
